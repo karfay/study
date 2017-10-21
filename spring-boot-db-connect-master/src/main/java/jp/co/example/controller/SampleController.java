5d66f593ef08bc48fa4013964f61a74ae2908c56
karfay <karfay.abretta@gmail.com>
2017-10-21 15:48:52 +0900

    test upload

---
+++ spring-boot-db-connect-master/src/main/java/jp/co/example/controller/SampleController.java
@@ -0,0 +1,26 @@
+package jp.co.example.controller;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import jp.co.example.dao.SampleDao;
+import jp.co.example.entity.Sample;
+
+@RestController
+public class SampleController {
+
+    @Autowired
+    private SampleDao sampleDao;
+    
+    @RequestMapping(method = RequestMethod.GET)
+    @Transactional
+    public List<Sample> get() {
+        return sampleDao.findAll();
+    }
+}
